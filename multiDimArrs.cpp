43d24b6acf2b65fb1ed0eca0a92583cec05fddad
denzel-k <denzelk741@gmail.com>
2023-09-23 08:49:17 +0300

    initial commit

---
+++ multiDimArrs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+  string cars[][3] = {
+    {"Mustang", "Escape", "F-150"},
+    {"Corvette", "Equinox", "Silverado"},
+    {"Challenger", "Durango", "Ram-1500"}
+  };
+  
+  int rows = sizeof(cars)/sizeof(cars[0]);
+  int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+  
+  cout << "The following are cars manufuctured by ford, Chevrolet and Ram respectively: " << '\n';
+  
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+      cout << cars[i][j] << " ";
+    }
+    cout << '\n';
+  }
+  
+  return 0;
+}
